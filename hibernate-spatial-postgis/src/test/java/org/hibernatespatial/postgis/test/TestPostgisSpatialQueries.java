2766ae528d2bf8d89e8b1bf0841ce817ac2b5720
maesenka <maesenka@dbc8e70c-b77c-47d0-b4d5-0b8eeea60b86>
2007-10-29 19:11:43 +0000

    Updated to reflect the changes in delegate.

--- hibernate-spatial-postgis/src/test/java/org/hibernatespatial/postgis/test/TestPostgisSpatialQueries.java
+++ hibernate-spatial-postgis/src/test/java/org/hibernatespatial/postgis/test/TestPostgisSpatialQueries.java
@@ -1,5 +1,5 @@
 /**
- * $Id$
+ * $Id:TestPostgisSpatialQueries.java 40 2007-09-20 15:33:24Z maesenka $
  *
  * This file is part of Spatial Hibernate, an extension to the 
  * hibernate ORM solution for geographic data. 
@@ -25,7 +25,7 @@
  *
  * For more information, visit: http://www.cadrie.com/
  */
- 
+
 package org.hibernatespatial.postgis.test;
 
 import java.sql.Connection;
@@ -38,158 +38,198 @@ import org.junit.AfterClass;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
-
 public class TestPostgisSpatialQueries {
 
-    private final static String DBNAME = "test";
+	private final static String DBNAME = "test";
 
-    private static Connection conn;
+	private static Connection conn;
 
-    private static TestSpatialQueries delegate;
+	private static TestSpatialQueries delegate;
 
-    static {
-        String url = "jdbc:postgresql://localhost:5432/" + DBNAME;
-        try {
-            Class.forName("org.postgresql.Driver");
-            conn = DriverManager.getConnection(url, "postgres", "");
-            delegate = new TestSpatialQueries();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        // create the delegate
+	static {
+		String url = "jdbc:postgresql://localhost:5432/" + DBNAME;
+		try {
+			Class.forName("org.postgresql.Driver");
+			conn = DriverManager.getConnection(url, "postgres", "");
+			delegate = new TestSpatialQueries();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		// create the delegate
 
-    }
+	}
 
-    @BeforeClass
-    public static void setUpBeforeClass() throws Exception {
-        delegate.setUpBeforeClass(conn);
-    }
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+		delegate.setUpBeforeClass(conn);
+	}
 
-    @AfterClass
-    public static void tearDownAfterClass() throws Exception {
-        delegate.tearDownAfterClass();
-    }
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+		delegate.tearDownAfterClass();
+	}
 
-    @Test
-    public void testHGLGeometryType() throws Exception {
-        delegate.testHGLGeometryType();
-    }
+	@Test
+	public void testHQLGeometryType() throws Exception {
+		delegate.testHQLGeometryType();
+	}
 
-    @Test
-    public void testHQLAsBinary() throws Exception {
-        String sql = "select asbinary(l.geom) from linestringtest as l where id = ?";
-        delegate.testHQLAsBinary(sql);
-    }
+	@Test
+	public void testHQLAsBinary() throws Exception {
+		delegate.testHQLAsBinary();
+	}
 
-    @Test
-    public void testHQLAsTextLineString() throws Exception {
-        delegate.testHQLAsTextLineString();
-    }
+	@Test
+	public void testHQLAsText() throws Exception {
+		delegate.testHQLAsText();
+	}
 
-    @Test
-    public void testHQLDimensionLineString() throws Exception {
-        delegate.testHQLDimensionLineString();
-    }
+	@Test
+	public void testHQLDimension() throws Exception {
+		delegate.testHQLDimension();
+	}
 
-    @Test
-    public void testHQLEnvelope() throws Exception {
-        delegate.testHQLEnvelope();
-    }
+	@Test
+	public void testHQLEnvelope() throws Exception {
+		delegate.testHQLEnvelope();
+	}
 
-    @Test
-    public void testHQLIntersectsLineString() throws Exception {
-        String sqlString = "select count(*) from linestringtest where intersects(geomfromtext(?, 31370), geom)";
-        delegate.testHQLIntersectsLineString(sqlString);
-    }
+	@Test
+	public void testHQLIntersectsLineString() throws Exception {
+		String sqlString = "select count(*) from $table$ where intersects(geomfromtext(?, 31370), geom)";
+		delegate.testHQLIntersectsLineString(sqlString);
+	}
 
-    @Test
-    public void testHQLIsEmpty() throws Exception {
-        String sql = "select isempty(geom) from linestringtest where id = ? and geom is not null";
-        delegate.testHQLIsEmpty(sql);
-    }
+	@Test
+	public void testHQLIsEmpty() throws Exception {
+		String sql = "select count(*) from $table$ where geom is not null and isempty(geom)";
+		delegate.testHQLIsEmpty(sql);
+	}
 
-    @Test
-    public void testHQLIsSimple() throws Exception {
-        String sql = "select issimple(geom) from linestringtest where id = ? and geom is not null";
-        delegate.testHQLIsSimple(sql);
-    }
+	@Test
+	public void testHQLIsSimple() throws Exception {
+		String sql = "select  count(*) from $table$ where geom is not null and issimple(geom)";
+		delegate.testHQLIsSimple(sql);
+	}
 
-    @Test
-    public void testHQLOverlapsLineString() throws Exception {
-        String sql = "select count(*) from linestringtest where overlaps(geomfromtext(?, 31370), geom) and geom is not null";
-        delegate.testHQLOverlapsLineString(sql);
-    }
+	@Test
+	public void testHQLOverlaps() throws Exception {
+		String sql = "select count(*) from $table$ where overlaps(geomfromtext(?, 31370), geom) and geom is not null";
+		delegate.testHQLOverlaps(sql);
+	}
 
-    @Test
-    public void testHQLSRID() throws Exception {
-        delegate.testHQLSRID();
-    }
+	@Test
+	public void testHQLSRID() throws Exception {
+		delegate.testHQLSRID();
+	}
 
-    @Test
-    public void testLineStringFiltering() throws Exception {
-        String sql = "select count(*) from linestringtest where geom && geomFromText(?,31370)";
-        delegate.testLineStringFiltering(sql);
-    }
+	@Test
+	public void testFiltering() throws Exception {
+		String sql = "select count(*) from $table$ where geom && geomFromText(?,31370)";
+		delegate.testFiltering(sql);
+	}
 
-    @Test
-    public void testMultiLineStringFiltering() throws Exception {
-        String sql = "select count(*) from multilinestringtest where geom && geomFromText(?,31370)";
-        delegate.testMultiLineStringFiltering(sql);
-    }
+	@Test
+	public void testContains() throws Exception {
+		String sql = "select count(*) from $table$ where geom && geomFromText(?,31370) and contains(geom, geomFromText(?, 31370))";
+		delegate.testContains(sql);
+	}
 
-    @Test
-    public void testPolygonFiltering() throws Exception {
-        String sql = "select count(*) from polygontest where geom && geomFromText(?,31370)";
-        delegate.testPolygonFiltering(sql);
-    }
+	@Test
+	public void testCrosses() throws Exception {
+		String sql = "select count(*) from $table$ where geom && geomFromText(?,31370) and crosses(geom, geomFromText(?, 31370))";
+		delegate.testCrosses(sql);
+	}
 
-    public static junit.framework.Test suite() {
-        return new JUnit4TestAdapter(TestPostgisSpatialQueries.class);
-    }
+	@Test
+	public void testDisjoint() throws Exception {
+		String sql = "select count(*) from $table$ where disjoint(geom, geomFromText(?, 31370))";
+		delegate.testDisjoint(sql);
+	}
 
-    @Test
-    public void testHQLBoundary() throws Exception {
-        delegate.testHQLBoundary();
-    }
+	@Test
+	public void testHQLDisjoint() throws Exception {
+		String sql = "select count(*) from $table$ where disjoint(geomfromtext(?, 31370), geom) and geom is not null";
+		delegate.testHQLDisjoint(sql);
+	}
 
-    @Test
-    public void testHQLRelateLineString() throws Exception {
-        delegate.testHQLRelateLineString();
-    }
+	@Test
+	public void testEquals() throws Exception {
+		String sql = "select count(*) from $table$ where geom && geomFromText(?,31370) and equals(geom, geomFromText(?, 31370))";
+		delegate.testEquals(sql);
+	}
 
-    @Test
-    public void testHQLDistance() throws Exception {
-        delegate.testHQLDistance();
-    }
+	@Test
+	public void testIntersects() throws Exception {
+		String sql = "select count(*) from $table$ where geom && geomFromText(?,31370) and intersects(geom, geomFromText(?, 31370))";
+		delegate.testIntersects(sql);
+	}
 
-    @Test
-    public void testHQLBuffer() throws Exception {
-        delegate.testHQLBuffer();
-    }
+	@Test
+	public void testOverlaps() throws Exception {
+		String sql = "select count(*) from $table$ where geom && geomFromText(?,31370) and overlaps(geom, geomFromText(?, 31370))";
+		delegate.testOverlaps(sql);
+	}
 
-    @Test
-    public void testHQLConvexHull() throws Exception {
-        delegate.testHQLConvexHull();
-    }
+	@Test
+	public void testTouches() throws Exception {
+		String sql = "select count(*) from $table$ where geom && geomFromText(?,31370) and touches(geom, geomFromText(?, 31370))";
+		delegate.testTouches(sql);
+	}
 
-    @Test
-    public void testHQLIntersection() throws Exception {
-        delegate.testHQLIntersection();
-    }
+	@Test
+	public void testWithin() throws Exception {
+		String sql = "select count(*) from $table$ where geom && geomFromText(?,31370) and within(geom, geomFromText(?, 31370))";
+		delegate.testWithin(sql);
+	}
 
-    @Test
-    public void testHQLDifference() throws Exception {
-        delegate.testHQLDifference();
-    }
+	public static junit.framework.Test suite() {
+		return new JUnit4TestAdapter(TestPostgisSpatialQueries.class);
+	}
 
-    @Test
-    public void testHQLSymDifference() throws Exception {
-        delegate.testHQLSymDifference();
-    }
+	@Test
+	public void testHQLBoundary() throws Exception {
+		delegate.testHQLBoundary();
+	}
 
-    @Test
-    public void testHQLUnion() throws Exception {
-        delegate.testHQLUnion();
-    }
+	@Test
+	public void testHQLRelateLineString() throws Exception {
+		delegate.testHQLRelateLineString();
+	}
+
+	@Test
+	public void testHQLDistance() throws Exception {
+		delegate.testHQLDistance();
+	}
+
+	@Test
+	public void testHQLBuffer() throws Exception {
+		delegate.testHQLBuffer();
+	}
+
+	@Test
+	public void testHQLConvexHull() throws Exception {
+		delegate.testHQLConvexHull();
+	}
+
+	@Test
+	public void testHQLIntersection() throws Exception {
+		delegate.testHQLIntersection();
+	}
+
+	@Test
+	public void testHQLDifference() throws Exception {
+		delegate.testHQLDifference();
+	}
+
+	@Test
+	public void testHQLSymDifference() throws Exception {
+		delegate.testHQLSymDifference();
+	}
+
+	@Test
+	public void testHQLUnion() throws Exception {
+		delegate.testHQLUnion();
+	}
 
 }
