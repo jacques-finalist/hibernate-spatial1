604e3a3533e0cf28365b353b9245830172f68ab7
maesenka <maesenka@dbc8e70c-b77c-47d0-b4d5-0b8eeea60b86>
2007-11-12 21:23:46 +0000

    * POM has now explicit dependency on test-classes * Header files updated.

--- hibernate-spatial-postgis/src/main/java/org/hibernatespatial/postgis/DialectProvider.java
+++ hibernate-spatial-postgis/src/main/java/org/hibernatespatial/postgis/DialectProvider.java
@@ -1,14 +1,15 @@
 /**
  * $Id$
  *
- * This file is part of Spatial Hibernate, an extension to the 
+ * This file is part of Hibernate Spatial, an extension to the 
  * hibernate ORM solution for geographic data. 
  *  
+ * Copyright © 2007 Geovise BVBA
  * Copyright © 2007 K.U. Leuven LRD, Spatial Applications Division, Belgium
  *
  * This work was partially supported by the European Commission, 
  * under the 6th Framework Programme, contract IST-2-004688-STP.
- * 
+ *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
  * License as published by the Free Software Foundation; either
@@ -23,16 +24,14 @@
  * License along with this library; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  *
- * For more information, visit: http://www.cadrie.com/
+ * For more information, visit: http://www.hibernatespatial.org/
  */
- 
 package org.hibernatespatial.postgis;
 
 import java.util.Map;
 
 import org.hibernatespatial.SpatialDialect;
 import org.hibernatespatial.spi.SpatialDialectProvider;
-
 
 /**
  * PostGIS DialectProvider
@@ -41,37 +40,37 @@ import org.hibernatespatial.spi.SpatialDialectProvider;
  */
 public class DialectProvider implements SpatialDialectProvider {
 
-    /*
-     * (non-Javadoc)
-     * 
-     * @see org.hibernatespatial.spi.SpatialDialectProvider#createSpatialDialect(java.lang.String,
-     *      java.util.Map)
-     */
-    public SpatialDialect createSpatialDialect(String dialect, Map map) {
-        if (dialect.equals(PostgisDialect.class.getCanonicalName())
-                || dialect.equals("org.hibernate.dialect.PostgreSQLDialect")
-                || dialect.equals("postgis"))
-            return new PostgisDialect();
-        else
-            return null;
-    }
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see org.hibernatespatial.spi.SpatialDialectProvider#createSpatialDialect(java.lang.String,
+	 *      java.util.Map)
+	 */
+	public SpatialDialect createSpatialDialect(String dialect, Map map) {
+		if (dialect.equals(PostgisDialect.class.getCanonicalName())
+				|| dialect.equals("org.hibernate.dialect.PostgreSQLDialect")
+				|| dialect.equals("postgis"))
+			return new PostgisDialect();
+		else
+			return null;
+	}
 
-    /*
-     * (non-Javadoc)
-     * 
-     * @see org.hibernatespatial.spi.SpatialDialectProvider#getDefaultDialect()
-     */
-    public SpatialDialect getDefaultDialect() {
-        return new PostgisDialect();
-    }
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see org.hibernatespatial.spi.SpatialDialectProvider#getDefaultDialect()
+	 */
+	public SpatialDialect getDefaultDialect() {
+		return new PostgisDialect();
+	}
 
-    /*
-     * (non-Javadoc)
-     * 
-     * @see org.hibernatespatial.spi.SpatialDialectProvider#getSupportedDialects()
-     */
-    public String[] getSupportedDialects() {
-        return new String[] { PostgisDialect.class.getCanonicalName() };
-    }
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see org.hibernatespatial.spi.SpatialDialectProvider#getSupportedDialects()
+	 */
+	public String[] getSupportedDialects() {
+		return new String[] { PostgisDialect.class.getCanonicalName() };
+	}
 
 }
--- hibernate-spatial-postgis/src/test/java/org/hibernatespatial/postgis/test/TestPostgisCRUD.java
+++ hibernate-spatial-postgis/src/test/java/org/hibernatespatial/postgis/test/TestPostgisCRUD.java
@@ -1,14 +1,15 @@
 /**
  * $Id$
  *
- * This file is part of Spatial Hibernate, an extension to the 
+ * This file is part of Hibernate Spatial, an extension to the 
  * hibernate ORM solution for geographic data. 
  *  
+ * Copyright © 2007 Geovise BVBA
  * Copyright © 2007 K.U. Leuven LRD, Spatial Applications Division, Belgium
  *
  * This work was partially supported by the European Commission, 
  * under the 6th Framework Programme, contract IST-2-004688-STP.
- * 
+ *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
  * License as published by the Free Software Foundation; either
@@ -23,9 +24,8 @@
  * License along with this library; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  *
- * For more information, visit: http://www.cadrie.com/
+ * For more information, visit: http://www.hibernatespatial.org/
  */
- 
 package org.hibernatespatial.postgis.test;
 
 import junit.framework.JUnit4TestAdapter;
@@ -35,37 +35,36 @@ import org.junit.AfterClass;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
-
 public class TestPostgisCRUD {
 
-    private final static TestCRUD delegate;
+	private final static TestCRUD delegate;
 
-    static {
-        delegate = new TestCRUD();
-    }
+	static {
+		delegate = new TestCRUD();
+	}
 
-    @BeforeClass
-    public static void setUpBeforeClass() throws Exception {
-        delegate.setUpBeforeClass();
-    }
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+		delegate.setUpBeforeClass();
+	}
 
-    @AfterClass
-    public static void tearDownAfterClass() throws Exception {
-        delegate.tearDownAfterClass();
-    }
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+		delegate.tearDownAfterClass();
+	}
 
-    @Test
-    public void testSaveLineStringEntity() throws Exception {
-        delegate.testSaveLineStringEntity();
-    }
+	@Test
+	public void testSaveLineStringEntity() throws Exception {
+		delegate.testSaveLineStringEntity();
+	}
 
-    @Test
-    public void testSaveNullLineStringEntity() throws Exception {
-        delegate.testSaveNullLineStringEntity();
-    }
+	@Test
+	public void testSaveNullLineStringEntity() throws Exception {
+		delegate.testSaveNullLineStringEntity();
+	}
 
-    public static junit.framework.Test suite() {
-        return new JUnit4TestAdapter(TestPostgisCRUD.class);
-    }
+	public static junit.framework.Test suite() {
+		return new JUnit4TestAdapter(TestPostgisCRUD.class);
+	}
 
 }
